8b277032c505bd9e15d0c572f359bcdde1f2eb8d
tkach <tkachenkoalex66@gmail.com>
2025-01-11 23:00:26 +0500

    11

--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ class Settings:
     def __init__(self):
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (230,130,230)
+        self.bg_color = (255,255,255)
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -2,17 +2,26 @@ import pygame
 class Ship:
 
     def __init__(self, ai_game):
+        """Ининциализирует корабль и задает его начальную позицию"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load('image/ship1.png')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра коробля
+        self.x = float(self.rect.x)
+        # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left >0:
+            self.x -= self.settings.ship_speed
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
     def blitme(self):
         self.screen.blit(self.image, self.rect)
